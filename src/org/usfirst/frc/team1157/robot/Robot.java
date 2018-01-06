2338dc44d6d49d49c7b02ee29132ea592ff60b3f
Julien Sloan <julislo123@gmail.com>
2018-01-06 14:52:41 -0700

    🥨

--- src/org/usfirst/frc/team1157/robot/Robot.java
+++ src/org/usfirst/frc/team1157/robot/Robot.java
@@ -12,8 +12,7 @@ import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import org.usfirst.frc.team1157.robot.commands.ExampleCommand;
-import org.usfirst.frc.team1157.robot.subsystems.ExampleSubsystem;
+import org.usfirst.frc.team1157.robot.subsystems.DriveTrain;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -23,8 +22,7 @@ import org.usfirst.frc.team1157.robot.subsystems.ExampleSubsystem;
  * project.
  */
 public class Robot extends TimedRobot {
-	public static final ExampleSubsystem kExampleSubsystem
-			= new ExampleSubsystem();
+	public static final DriveTrain DriveTrain = new DriveTrain();
 	public static OI m_oi;
 
 	Command m_autonomousCommand;
@@ -37,7 +35,6 @@ public class Robot extends TimedRobot {
 	@Override
 	public void robotInit() {
 		m_oi = new OI();
-		m_chooser.addDefault("Default Auto", new ExampleCommand());
 		// chooser.addObject("My Auto", new MyAutoCommand());
 		SmartDashboard.putData("Auto mode", m_chooser);
 	}
