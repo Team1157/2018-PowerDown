65e2718d39580bbc14d3d1e63a58e9e908c66043
Shark <Shark@BOHC5F0D9.admin.bvsd.k12.co.us>
2018-01-13 14:55:46 -0700

    set up the motors and started getting joystick drive ready

--- src/org/usfirst/frc/team1157/robot/commands/DriveJoystick.java
+++ src/org/usfirst/frc/team1157/robot/commands/DriveJoystick.java
@@ -1,4 +1,7 @@
 package org.usfirst.frc.team1157.robot.commands;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import org.usfirst.frc.team1157.robot.Robot;
+import org.usfirst.frc.team1157.robot.subsystems.DriveTrain;
 
 import edu.wpi.first.wpilibj.command.Command;
 
@@ -6,31 +9,40 @@ import edu.wpi.first.wpilibj.command.Command;
  *
  */
 public class DriveJoystick extends Command {
+	xOI.stick2.getX()
 
-    public DriveJoystick() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
+    public DriveJoystick() 
+    {
+        requires(Robot.DriveTrain);
     }
 
     // Called just before this Command runs the first time
-    protected void initialize() {
+    protected void initialize() 
+    {
+   
+    }
     }
 
     // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
+    protected void execute() 
+    {
+    	DifferentialDrive.arcadeDrive(double xSpeed, double zRotation, boolean squaredInputs) {
     }
 
     // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
+    protected boolean isFinished() 
+    {
         return false;
     }
 
     // Called once after isFinished returns true
-    protected void end() {
+    protected void end() 
+    {
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
-    protected void interrupted() {
+    protected void interrupted() 
+    {
     }
 }
