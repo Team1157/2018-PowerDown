382103647e6fd70a57536a27f4c08094b5e44a69
Shark <Shark@BOHC5F0D9.admin.bvsd.k12.co.us>
2018-01-13 11:29:30 -0700

    Confused

--- src/org/usfirst/frc/team1157/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team1157/robot/subsystems/DriveTrain.java
@@ -1,15 +1,16 @@
 package org.usfirst.frc.team1157.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
-
+import com.ctre.CANTalon;
 /**
  *
  */
 public class DriveTrain extends Subsystem {
 
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
 
+
+    CANTalon RightMotor;
+    CANTalon LeftMotor;
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
